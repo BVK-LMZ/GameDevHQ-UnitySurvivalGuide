b3611a5eff400840b5814be33848b5e666840b33
LoMZo <78233321+BVK-LMZ@users.noreply.github.com>
2023-10-17 00:22:18 -0400

    Update TipCalculator.cs

--- section2:Variables/TipCalculator.cs
+++ section2:Variables/TipCalculator.cs
@@ -1,1 +1,29 @@
+using UnityEngine;
+
+public class HelloWorld : MonoBehaviour
+{
+    float pricePriorPatronage;
+    float tipAmount;
+
+    void Start()
+    {
+        bool isLucky = CoinFlip();
+        Debug.Log(isLucky);  // "L" in "Log" should be uppercase
+
+        if (isLucky)    // if true
+        {
+            tipAmount = pricePriorPatronage * 0.20f;  // Use "f" to indicate a float value
+        }
+        else
+        {
+            tipAmount = pricePriorPatronage * 0.10f;  // Use "f" to indicate a float value
+        }
+    }
+
+    public bool CoinFlip()
+    {
+        System.Random random = new System.Random();  // Use "System.Random" to create a random object
+        return random.Next(0, 2) == 1;  // true if 1, false if 0
+    }
+}
 
