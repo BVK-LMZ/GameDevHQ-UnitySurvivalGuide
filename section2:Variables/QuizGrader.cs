0b1a907900f5b6d35acead1f7001524b70cf5813
LoMZo <78233321+BVK-LMZ@users.noreply.github.com>
2023-10-20 16:40:28 -0400

    Update QuizGrader.cs

--- section2:Variables/QuizGrader.cs
+++ section2:Variables/QuizGrader.cs
@@ -2,56 +2,46 @@ using UnityEngine;
 
 public class Testa : MonoBehaviour
 {
-    int[] testScores; // Declare the array at the class level.
-    int AverageScore;
+    int[] testScores; // Declare the array at the class level
+    int averageScore; // Declare a variable to hold the final answer
 
-    // Function to generate random test scores
+    // Function to generate a specified number of random test scores
     void GenerateTests(int numberOfScores)
     {
-        testScores = new int[numberOfScores]; // Assign the generated scores to the class-level array.
-
+        // Initialize the testScores array with the specified number of elements.
+        testScores = new int[numberOfScores];
         for (int i = 0; i < numberOfScores; i++)
         {
-            testScores[i] = Random.Range(1, 101); // Generate random score between 1 and 100
+            testScores[i] = Random.Range(1, 101); // Generate random scores between 1 and 100
         }
 
-        // Print the generated test scores to the console
+        // Debug log to check the contents of the class-level array
         for (int i = 0; i < numberOfScores; i++)
         {
             Debug.Log("Test Score " + (i + 1) + ": " + testScores[i]);
         }
     }
 
-
-    //No params because this class level function relies on the class level array
-    void ReturnAverage()
-    {   
-
-        //Use generate tests to populate the array before attempting to return the average
+    // No parameters because this class-level function relies on the class-level array
+    void CalculateAverage()
+    {
         if (testScores == null)
         {
-            Debug.LogError("Test scores array is not initialized. Call GenerateTests first.");
+            Debug.LogError("Test scores array is null.");
             return;
         }
 
-
-        //---- Get SUM of testScores Array
-        int numberOfTests = testScores.Length; // Get the number of tests from the array.
+        // Calculate the sum of testScores array
+        int numberOfTests = testScores.Length;
         int sum = 0;
         for (int i = 0; i < numberOfTests; i++)
         {
             sum += testScores[i];
         }
-        //----
 
-
-        float average = (float)sum / numberOfTests; // Calculate the average as a float 
-
-
-
-        AverageScore = average;                         //Once calculated set the value of the class level variable to this 
-        Debug.Log("Average Test Score: " + average);    //Print it 
-
+        float average = (float)sum / numberOfTests; // Calculate the average as a float
+        averageScore = Mathf.RoundToInt(average); // Round the average to the nearest integer
+        Debug.Log("Average Test Score: " + averageScore);
     }
 
     void Start()
@@ -61,8 +51,8 @@ public class Testa : MonoBehaviour
         // Call the GenerateTests function with the number of scores you want (e.g., 5).
         GenerateTests(5);
 
-        // Call the ReturnAverage function to calculate and print the average.
-        ReturnAverage();
+        // Call the CalculateAverage function to calculate and print the average.
+        CalculateAverage();
     }
 }
 
