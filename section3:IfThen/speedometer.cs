6751d9e93b66a97756362579dfd77d4fe277334d
LoMZo <78233321+BVK-LMZ@users.noreply.github.com>
2023-10-24 18:56:33 -0400

    Update speedometer.cs

--- section3:IfThen/speedometer.cs
+++ section3:IfThen/speedometer.cs
@@ -1,2 +1,41 @@
+using UnityEngine;
 
-yew
+public class SomeClass : MonoBehaviour
+{
+    private float acceleration = 0;
+
+    void Update()
+    {
+        HandleMovement();
+        MovementAlerts();
+    }
+
+    void HandleMovement()
+    {
+        if (Input.GetKey(KeyCode.W))
+        {
+            acceleration += 1;
+        }
+        if (Input.GetKey(KeyCode.S))
+        {
+            acceleration -= 1;
+        }
+    }
+
+
+    void MovementAlerts()
+    {
+        if (acceleration >= 20)
+        {
+            Debug.Log("Too fast");
+        }
+        else if (acceleration == 0)
+        {
+            Debug.Log("Stopped");
+        }
+    }
+
+
+
+
+}
