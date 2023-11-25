d9d911b8d3d4acd7a6aaf61065b65d94f2fc945b
LoMZo <78233321+BVK-LMZ@users.noreply.github.com>
2023-11-25 12:37:26 -0500

    Update Car Class Basics.cs

--- Car Class Basics.cs
+++ Car Class Basics.cs
@@ -4,23 +4,6 @@ class Program
 {
     static void Main()
     {
-        Console.WriteLine("Hello, World!");
-        Console.WriteLine("Hello, World!2");
-        Console.WriteLine("Hello, World!3");
-
-        try
-        {
-            throw new Exception("This is a forced exception."); //Stop hello world 4 from printing
-            Console.WriteLine("DEMON MESSAGE <OUGHT NOT BE SEEN>"); 
-        }
-        catch(Exception e)
-        {
-            Console.WriteLine("Hello, World!4"); //This will print
-            Console.WriteLine("Caught an exception: " + e.Message);
-            Console.WriteLine("beans"); //This will NNOT print
-        }
-
-
         car myCar = new car("Jeep", 0);
         myCar.drive();
     }
